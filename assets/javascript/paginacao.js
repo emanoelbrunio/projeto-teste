db69a9c95fe76461b07d44d178985682d2761d0f
emanoel brunio <emanoelbrunio@gmail.com>
2022-03-21 15:44:07 -0300

    organizando arquivos em pastas

--- assets/javascript/paginacao.js
+++ assets/javascript/paginacao.js
@@ -67,27 +67,5 @@ window.addEventListener("scroll", function(){
         this.document.querySelector(".a_ini").classList.remove("a_forte");
         this.document.querySelector(".a_qs").classList.remove("a_forte");
         this.document.querySelector(".a_ap").classList.remove("a_forte");
-    } 
-       
-    // console.log(alturaAP);
-    // console.log(alturaFalec);
-    // console.log(alturaInico);
-    
+    }  
 });
-
-// window.addEventListener("load", function(){
-//     const largura = this.window.screen.width;
-    
-//     if (largura <= 850) {
-//         this.window.addEventListener("scroll", function(){
-
-//             if (this.window.scrollY > 100){  
-//                 this.document.querySelector(".header_logo").classList.add("invi"); 
-//             }
-//             else {
-//                 this.document.querySelector(".header_logo").classList.remove("invi")
-//             }
-//         });
-//     }
-    
-// });
